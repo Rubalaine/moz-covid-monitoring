4e0627c5dd7dbb1d29c91cc77963c01b58d1f26b
Kelven Rubalaine <kelvenizzy@gmail.com>
2020-07-10 13:25:40 +0200

    finalmente

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
-dotenv = require("dotenv");
+// dotenv = require("dotenv");
 
 process.on("uncaughtException", (err) => {
   console.log("UNCAUGHT EXCEPTION!  Shutting down...");
   console.log(err.name, err);
   process.exit(1);
 });
-dotenv.config({ path: "./config.env" });
+// dotenv.config({ path: "./config.env" });
 const DB =
   "mongodb+srv://kelven-admin:eu-sou-gostoso@covid-monitor-cluster.tjpr0.mongodb.net/covid-monitor?retryWrites=true&w=majority";
 mongoose
@@ -20,8 +20,8 @@ mongoose
     console.log("conectado com sucesso");
   });
 const app = require("./app");
-
-const port = procces.env.PORT || 8080;
+console.log("dotenv", process.env);
+const port = process.env.PORT || 8080;
 server = app.listen(port, () => {
   console.log("listening on port: ", port);
 });
