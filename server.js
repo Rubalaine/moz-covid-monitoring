9a008fd599aacf8afc7942ece5e47cd829981bfc
Kelven Rubalaine <kelvenizzy@gmail.com>
2020-07-10 08:21:42 +0200

    atualizado novamente

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
+dotenv = require("dotenv");
 
 process.on("uncaughtException", (err) => {
   console.log("UNCAUGHT EXCEPTION!  Shutting down...");
   console.log(err.name, err.message);
   process.exit(1);
 });
-
+dotenv.config({ path: "./config.env" });
 const DB =
   "mongodb+srv://kelven-admin:eu-sou-gostoso@covid-monitor-cluster.tjpr0.mongodb.net/covid-monitor?retryWrites=true&w=majority";
 mongoose
